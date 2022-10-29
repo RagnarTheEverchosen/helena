cb50d062446121beeb5c6e283eb061656e180c25
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-10-29 15:28:25 +0200

    added custom presence

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,7 @@
+import { ActivityType } from 'discord.js'
 import { event } from '../utils'
 
 export default event('ready', ({ log }, client) => {
 	log(`Logged in as ${client.user.tag}`)
+	client.user.setPresence({ activities: [{ type: ActivityType.Watching, name: 'over MENDELU' }], status: 'online' })
 })
