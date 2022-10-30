8feae5c7d912cff340b75abc801dff09cf25d276
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-10-30 09:46:13 +0100

    register command

--- src/commands/auth/index.ts
+++ src/commands/auth/index.ts
@@ -1,6 +1,8 @@
 import { category } from '../../utils'
 import verify from './verify'
+import register from './register'
 
 export default category('Auth', [
 	verify,
+	register,
 ])
---
+++ src/commands/auth/register.ts
@@ -0,0 +1,57 @@
+import { Colors, EmbedBuilder, SlashCommandBuilder, TextChannel } from 'discord.js';
+import { command } from '../../utils';
+
+const meta = new SlashCommandBuilder()
+	.setName('register')
+	.setDescription('Registeres a MENDELU student.')
+	.addNumberOption((option) => 
+		option
+			.setName('token')
+			.setDescription('Verification token.')
+			.setRequired(true)
+	);
+
+export default command(meta, async ({ interaction }) => {
+	const token = interaction.options.getNumber('token');
+
+	await interaction.deferReply({
+		ephemeral: true,
+	});
+
+	//TODO: Validate token
+	const isTokenValid = false;
+
+	const response = new EmbedBuilder();
+
+	if (!isTokenValid) {
+		response
+			.setAuthor({ name: 'Verification', iconURL: 'https://cdn4.iconfinder.com/data/icons/basic-ui-colour/512/ui-41-512.png' })
+			.setTitle('Registration Failed')
+			.setDescription('Token you provided is invalid. Please try again.')
+			.setColor(Colors.Red)
+			.setFooter({ text: 'If you cannot register, send message to Fouss#3807' });
+		
+
+		return await interaction.editReply({
+			embeds: [response]
+		});
+	}
+
+	response
+		.setAuthor({ name: 'Verification', iconURL: 'https://cdn4.iconfinder.com/data/icons/basic-ui-colour/512/ui-41-512.png' })
+		.setTitle('Registration Successful')
+		.setDescription('Welcome to the MENDELU Discord Server')
+		.setColor(Colors.Green)
+
+	await interaction.editReply({
+		embeds: [response]
+	});
+
+	const registerEmbed = new EmbedBuilder()
+		.setDescription(`<@${interaction.member?.user.id}> registered successfuly`)
+		.setColor(Colors.Green)
+
+	const logChannel = interaction.guild?.channels.cache.get('1036030834613956712');
+	(logChannel as TextChannel).send({ embeds: [registerEmbed] })
+
+});
