f11e7b3947c716a225203972bbe9b98697285a25
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2023-01-05 20:15:06 +0100

    add field

--- src/commands/general/index.ts
+++ src/commands/general/index.ts
@@ -1,8 +1,10 @@
 import { category } from '../../utils';
 import addCourse from './addCourse';
 import removeCourse from './removeCourse';
+import addField from './addField';
 
 export default category('General', [
 	addCourse,
 	removeCourse,
+	addField
 ]);
