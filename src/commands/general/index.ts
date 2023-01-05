23fc8ee9c07822ea4deeab2221ec33a3589c698e
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2023-01-05 17:34:33 +0100

    clean up

--- src/commands/general/index.ts
+++ src/commands/general/index.ts
@@ -1,10 +1,8 @@
 import { category } from '../../utils';
 import addCourse from './addCourse';
 import removeCourse from './removeCourse';
-import help from './help';
 
 export default category('General', [
 	addCourse,
 	removeCourse,
-	help
 ]);
--- src/events/interactionCreate/index.ts
+++ src/events/interactionCreate/index.ts
@@ -1,10 +1,8 @@
 import { Event } from '../../types';
 import commands from './commands';
-import help from './help';
 
 const events: Event<any>[] = [
 	commands,
-	help,
 ];
 
 export default events;
