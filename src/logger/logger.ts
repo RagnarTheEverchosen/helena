8959efd92811d86f59326334f9d72d2249a3f0c5
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2023-01-06 11:12:33 +0100

    added timestamp to loogging

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -4,33 +4,35 @@ export class Logger {
 	// Log a message at the specified level
 	private static log(level: string, primaryMessage: string, ...supportingData: any[]) {
 	
-	  // If supporting data is provided, log it along with the message
-	  if (supportingData.length > 0) {
-		console.log(`[${level}] ${primaryMessage}`, supportingData);
-	  } else {
-		// Otherwise, just log the message
-		console.log(`[${level}] ${primaryMessage}`);
-	  }
+		const timestamp = new Date().toISOString();
+
+	  	// If supporting data is provided, log it along with the message
+	  	if (supportingData.length > 0) {
+			console.log(`[${level}][${timestamp}] ${primaryMessage}`, supportingData);
+	  	} else {
+			// Otherwise, just log the message
+			console.log(`[${level}][${timestamp}] ${primaryMessage}`);
+	  	}
 	}
 	
 	// Log a debug message
 	public static debug(primaryMessage: string, ...supportingData: any[]) {
-	  this.log('DEBUG', primaryMessage, ...supportingData);
+	  	this.log('DEBUG', primaryMessage, ...supportingData);
 	}
   
 	// Log a warning message
 	public static warn(primaryMessage: string, ...supportingData: any[]) {
-	  this.log('WARNING', primaryMessage, ...supportingData);
+	  	this.log('WARNING', primaryMessage, ...supportingData);
 	}
   
 	// Log an error message
 	public static error(primaryMessage: string, ...supportingData: any[]) {
-	  this.log('ERROR', primaryMessage, ...supportingData);
+	  	this.log('ERROR', primaryMessage, ...supportingData);
 	}
   
 	// Log an info message
 	public static info(primaryMessage: string, ...supportingData: any[]) {
-	  this.log('INFO', primaryMessage, ...supportingData);
+	  	this.log('INFO', primaryMessage, ...supportingData);
 	}
 
   }
